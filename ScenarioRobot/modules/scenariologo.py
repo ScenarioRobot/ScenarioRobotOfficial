508058e34463ab11517901d9c6ee42faf1e3acda
Team Scenario <98752189+ScenarioRobot@users.noreply.github.com>
2022-02-02 01:48:24 +0530

    umm

--- ScenarioRobot/modules/scenariologo.py
+++ ScenarioRobot/modules/scenariologo.py
@@ -28,7 +28,7 @@ async def lego(event):
         img = Image.open(io.BytesIO(requests.get(randc).content))
         draw = ImageDraw.Draw(img)
         image_widthz, image_heightz = img.size
-        fnt = glob.glob("./aries/arieslogo/*")
+        fnt = glob.glob("./ScenarioRobot/scenariologo/*")
         randf = random.choice(fnt)
         font = ImageFont.truetype(randf, 120)
         w, h = draw.textsize(text, font=font)
