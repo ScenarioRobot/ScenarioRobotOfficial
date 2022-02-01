01f902c81c234d079649cbcb712c78991fa5e3c3
Team Scenario <98752189+ScenarioRobot@users.noreply.github.com>
2022-02-02 03:12:19 +0530

    minor changes

--- ScenarioRobot/modules/helper_funcs/misc.py
+++ ScenarioRobot/modules/helper_funcs/misc.py
@@ -68,9 +68,9 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 6:
         pairs = pairs[modulo_page * 6:6 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("「 MENU 」", callback_data="masha_back"),
-             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("🔻", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton(" ◻️ ", callback_data="scenario_back"),
+             EqInlineKeyboardButton("🔺", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
         pairs += [[EqInlineKeyboardButton("Home", callback_data="bot_start")]]
